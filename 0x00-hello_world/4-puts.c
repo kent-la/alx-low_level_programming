c9028e16c921232c0fe23ee0f09058e982a3ed0c
kent-la <fabiankent786@gmail.com>
2023-10-05 17:02:03 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+/*#!/bin/bash*/
 #include<stdio.h>
 /**
  * main -Entry point
@@ -6,6 +6,6 @@
  * Return: Always 0(Success)
  */
 int main(void)
-{    puts{"\"programming is like building a multilingual puzzle"};
+{    puts("\"programming is like building a multilingual puzzle");
 	return(0);
 }
