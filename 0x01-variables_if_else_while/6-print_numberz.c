ee81a4f64e0d64feb807b390265f05083cbda92b
kent-la <fabiankent786@gmail.com>
2023-10-09 15:08:51 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -prints numbers from 0to9.
+ *
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i<58; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return(0);
+}
