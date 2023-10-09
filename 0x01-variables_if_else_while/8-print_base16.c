e0235222e542d179c60d9ca20eed7efd80be5212
kent-la <fabiankent786@gmail.com>
2023-10-09 14:47:29 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints numbers between 0to9 and letters between ato f 
+ * Return:Always 0(success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i =97; i < 103; i ++ );
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return(0);
+}
